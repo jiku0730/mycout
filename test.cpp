c7cf41e04a1600458d8e0343f9e33b7f7d034623
jiku0730 <github@jiku.jp>
2026-08-12 09:27:23 +0900

    add: double の出力のテストを追加した。

--- test.cpp
+++ test.cpp
@@ -13,4 +13,11 @@ int main()
     mycout << '0' << '\n';
     mycout << ' ' << '!' << '\n';
     mycout << 'x' << 1 << 'y' << '\n';
+
+    mycout << 3.14 << '\n';
+    mycout << 0.5 << '\n';
+    mycout << -2.5 << '\n';
+    mycout << 100.0 << '\n';
+    mycout << 1e10 << '\n';
+    mycout << 1 << 0.25 << 'z' << '\n';
 }
